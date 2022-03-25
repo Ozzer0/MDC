8abe79154bb4f9df6d2a40f3c833a8024dfcfa53
Ozzer0 <96893483+Ozzer0@users.noreply.github.com>
2022-03-24 21:23:13 -0500

    Add files via upload

---
+++ MDC.bat
@@ -0,0 +1,29 @@
+@echo off
+title Minecraft Datapack Constucter ^| MDC
+setlocal EnableDelayedExpansion
+:questions
+echo What do you want the datapack name to be?
+set /p "Datapackname="
+echo What namespace Would you like to use?
+set /p "Datapacknamespace="
+:construct
+mkdir !Datapackname!
+cd !Datapackname!
+mkdir data
+echo {"pack":{"pack_format":9,"description":"!Datapackname!"}} > pack.mcmeta
+cd data
+mkdir !Datapacknamespace!
+cd !Datapacknamespace!
+mkdir advancements
+mkdir functions
+mkdir loot_tables
+mkdir recipes
+mkdir structures
+mkdir tags
+cd tags
+mkdir blocks
+mkdir entity_types
+mkdir fluids
+mkdir functions
+mkdir items
+exit /b
