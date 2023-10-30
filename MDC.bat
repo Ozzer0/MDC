fe92db4e7761b9af9a5a2bc4a884f2ec855574d4
Ozzer0 <96893483+Ozzer0@users.noreply.github.com>
2023-10-30 02:25:38 -0500

    Update MDC.bat
    Use a for loop to make bad code, decent code.

--- MDC.bat
+++ MDC.bat
@@ -2,29 +2,17 @@
 title Minecraft Datapack Constucter ^| MDC
 setlocal EnableDelayedExpansion
 :questions
-echo What do you want the datapack name to be?
-set /p "Datapackname="
-echo What namespace Would you like to use?
-set /p "Datapacknamespace="
+set /p "datapackname=Name: "
+set /p "datapacknamespace=Namespace: "
 :construct
-mkdir !Datapackname!
-cd !Datapackname!
+mkdir "!datapackname!"
+cd "!datapackname!"
 mkdir data
-echo {"pack":{"pack_format":9,"description":"!Datapackname!"}} > pack.mcmeta
+echo {"pack":{"pack_format":12,"description":"!datapackname!"}} > pack.mcmeta
 cd data
-mkdir !Datapacknamespace!
-cd !Datapacknamespace!
-mkdir advancements
-mkdir functions
-mkdir loot_tables
-mkdir recipes
-mkdir structures
-mkdir tags
+mkdir "!datapacknamespace!"
+cd "!datapacknamespace!"
+for %%A in (advancements functions loot_tables recipes structures tags) do mkdir %%A
 cd tags
-mkdir blocks
-mkdir entity_types
-mkdir predicates
-mkdir fluids
-mkdir functions
-mkdir items
+for %%A in (blocks entity_types predicates fluids functions items) do mkdir %%A
 exit /b
